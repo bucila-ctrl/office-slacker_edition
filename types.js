c4a8740e15a1ed3a0754231045371212ae236a2f
bucila-ctrl <wenzhao0524@gmail.com>
2025-12-13 06:05:50 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,7 @@
+export const initialGestureState = {
+  isWaving: false,     // Fishing
+  isFist: false,       // Catching
+  isPointing: false,   // Moving
+  handPresent: false,
+  move: { x: 0, y: 0 },
+};
